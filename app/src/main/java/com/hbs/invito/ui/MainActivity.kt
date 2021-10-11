1d8976f2bdcc3a7617409fd7169b885d0d88f941
ggujangi <asmwj3@naver.com>
2021-10-11 13:08:18 +0900

    first commit

---
+++ app/src/main/java/com/hbs/invito/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hbs.invito.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hbs.invito.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
