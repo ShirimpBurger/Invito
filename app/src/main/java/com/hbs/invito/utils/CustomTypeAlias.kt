b71a86809e27f8ec9c7d6bf4d1023e6a9f2b4cd5
hakzzang <mbh930312@naver.com>
2021-10-14 23:34:14 +0900

    feature : PermissionExtensions 및 TypeAlias 클래스 추가

---
+++ app/src/main/java/com/hbs/invito/utils/CustomTypeAlias.kt
@@ -0,0 +1,7 @@
+package com.hbs.invito.utils
+
+import com.hbs.invito.data.MediaStoreFolder
+import com.hbs.invito.data.MediaStoreImage
+
+typealias MediaStoreImageList = List<MediaStoreImage>
+typealias MediaStoreFolderList = List<MediaStoreFolder>
