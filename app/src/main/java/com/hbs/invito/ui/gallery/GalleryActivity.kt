61108c7c7814e583d0b1a3a5324c1f1d2961f35e
hakzzang <mbh930312@naver.com>
2021-10-24 19:03:17 +0900

    feature : GalleryActivity 개선

--- app/src/main/java/com/hbs/invito/ui/gallery/GalleryActivity.kt
+++ app/src/main/java/com/hbs/invito/ui/gallery/GalleryActivity.kt
@@ -1,13 +1,8 @@
 package com.hbs.invito.ui.gallery
 
 import android.Manifest
-import android.content.ContentValues
 import android.content.pm.PackageManager
-import android.graphics.ImageDecoder
-import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import android.provider.MediaStore
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Toast
@@ -16,43 +11,25 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.hbs.invito.databinding.ActivityGalleryBinding
 import com.hbs.invito.extensions.PermissionEnvironment
+import com.hbs.invito.extensions.ViewClickDataCallback
 import com.hbs.invito.extensions.checkAndRequestPermissions
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class GalleryActivity : AppCompatActivity() {
     private val binding by lazy { ActivityGalleryBinding.inflate(layoutInflater) }
     private val viewModel by lazy {
         ViewModelProvider(this).get(GalleryViewModel::class.java)
     }
-    private val galleryPictureAdapter = GalleryPictureAdapter()
+    private val galleryPictureAdapter = GalleryPictureAdapter().apply {
+        setHasStableIds(true)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         initView()
-
-        viewModel.images.observe(this, {
-            galleryPictureAdapter.submitList(it)
-        })
-
-        viewModel.folders.observe(this, {
-            val displayNames = it
-                .map { folder -> folder.displayName }
-                .toList()
-                .distinct()
-
-            val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, displayNames)
-            binding.tvGalleryPath.setAdapter(adapter)
-        })
-
-        lifecycleScope.launchWhenResumed {
-            checkAndRequestPermissions(
-                listOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-                PermissionEnvironment.READ_GALLERIES
-            )
-        }
+        observeViewModel()
+        requestPermissionForReadGalleries()
     }
 
     override fun onRequestPermissionsResult(
@@ -67,8 +44,8 @@ class GalleryActivity : AppCompatActivity() {
 
         if (requestCode == PermissionEnvironment.READ_GALLERIES) {
             lifecycleScope.launch {
-                viewModel.queryAllImage(contentResolver)
-                viewModel.queryImageFolders(contentResolver)
+                viewModel.queryAllImage(applicationContext)
+                viewModel.queryImageFolders(applicationContext)
             }
         }
     }
@@ -78,46 +55,38 @@ class GalleryActivity : AppCompatActivity() {
 
         }
 
-        binding.tvDownload.setOnClickListener {
-            clickDownloadView()
+        binding.ivGalleryClose.setOnClickListener {
+            finish()
         }
 
         binding.rvGalleryPictures.adapter = galleryPictureAdapter
-    }
-
-    private fun clickDownloadView() {
-        lifecycleScope.launch {
-            val uri = viewModel.images.value?.get(0)?.contentUri
-            uri?.let { insertImage(it) }
+        galleryPictureAdapter.callback = ViewClickDataCallback { _, item ->
+            viewModel.selectGalleryImage(item)
+            galleryPictureAdapter.notifyDataSetChanged()
         }
     }
 
-    private suspend fun insertImage(uri: Uri) {
-        //수정 예정
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            val collection = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
-            } else {
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-            }
+    private fun observeViewModel() {
+        viewModel.images.observe(this, {
+            galleryPictureAdapter.submitList(it)
+        })
 
-            withContext(Dispatchers.IO) {
-                val newFile = ContentValues().apply {
-                    put(MediaStore.Images.Media.DISPLAY_NAME, uri.path + "2")
-                    put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
-                }
+        viewModel.folders.observe(this, { folders ->
+            val adapter = ArrayAdapter(
+                this,
+                android.R.layout.simple_list_item_1,
+                folders.map { it.displayName }
+            )
+            binding.tvGalleryPath.setAdapter(adapter)
+        })
+    }
 
-                val newFileUri = contentResolver.insert(collection, newFile)
-                    ?: throw Exception("MediaStore Uri couldn't be created")
-
-                val bitmap = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                    ImageDecoder.decodeBitmap(ImageDecoder.createSource(contentResolver, uri))
-                } else {
-                    MediaStore.Images.Media.getBitmap(contentResolver, uri)
-                }
-
-                MediaStore.Images.Media.insertImage(contentResolver, bitmap, newFileUri.path, null)
-            }
+    private fun requestPermissionForReadGalleries() {
+        lifecycleScope.launchWhenResumed {
+            checkAndRequestPermissions(
+                listOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                PermissionEnvironment.READ_GALLERIES
+            )
         }
     }
 }
